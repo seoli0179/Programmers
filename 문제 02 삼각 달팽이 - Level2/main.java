b3f86b66cb965250ffa27067ad5849ac0ce81b79
김태수 <seoli0179@kakao.com>
2023-12-19 15:19:50 +0900

    Update main.java

--- 문제 02 삼각 달팽이 - Level2/main.java
+++ 문제 02 삼각 달팽이 - Level2/main.java
@@ -1,104 +1,57 @@
-import java.util.Arrays;
-
-// https://school.programmers.co.kr/learn/courses/30/lessons/68645
+// tip: each public class is put in its own file
 public class main
 {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/68645
     public static void main(String[] args)
     {
         Solution s = new Solution();
-        int[] answer = s.solution(2);
-
-        for(int i = 0; i< answer.length; i++)
-            System.out.print(answer[i]+" ");
+        int[] answer = s.solution(5);
+        for(int i : answer){
+            System.out.print(i + ",");
+        }
+        
     }
-
 }
 
 class Solution {
-        
+
+    private static final int[] dx = {0, 1, -1};
+    private static final int[] dy = {1, 0, -1};
+
     public int[] solution(int n) {
-        int[] answer = Cal(n);
-        return answer;
-    }
+        int[] answer = {};
 
-    public int[] Cal(int n) {
+        int[][] triangle = new int[n][n];        
 
-        int arr[][] = new int[n][n];
-        for (int i=0;i<n;i++){
-            Arrays.fill(arr[i], -1);
+        int x = 0;
+        int y = 0;
+        int d = 0;
+        int v = 1;
+
+        while (true){
+            triangle[y][x] = v++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if(nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0){
+                d = (d + 1) % 3;
+                nx = x + dx[d];
+                ny = y + dy[d];
+                if(nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0){
+                    break;
+                }
+            }
+            x = nx;
+            y = ny;
         }
 
-        int flag = 0;
-        int count = 0;
-        int i = 0;
-        int j = 0;
-
-        while(true){ 
-            if(n==1){
-                break;
-            } 
-            if(n==2){
-                arr[0][0] = 1;
-                arr[1][0] = 2;
-                count = 2;
-                i = 1;
-                j = 1;
-                break;
+        answer = new int[v - 1];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                answer[index++] = triangle[i][j];
             }
-            if(flag == 0){
-                if(i >= n-1){
-                    flag = 1;
-                }else if(arr[i+1][j] != -1){
-                    if(arr[i][j+1] != -1){
-                        break;
-                    }
-                    flag = 1; 
-                }
-                else{
-                    arr[i++][j] = ++count;          
-                }
-            }
-            else if(flag==1){
-                if(j >= n-1){
-                    flag = 2;
-                }else if(arr[i][j+1] != -1){
-                    if(arr[i-1][j-1] != -1){
-                        break;
-                    }
-                    flag = 2;
-                }
-                else{
-                    arr[i][j++] = ++count;
-                }
-            }
-            else if(flag==2){
-                if(arr[i-1][j-1] != -1){
-                    if(arr[i+1][j] != -1){
-                        break;
-                    }
-                    flag = 0;
-                }else{
-                    arr[i--][j--] = ++count;
-                }
-            }
-        }
-        arr[i][j] = ++count;   
-        
-        int answer[] = new int[count];
-        int arrCount = 0;
-
-        for(int k = 0; k < n; k++){
-            for(int l = 0; l < n; l++){
-                System.out.print(arr[k][l]+"    ");
-                if(arr[k][l]!=-1){
-                    answer[arrCount++] = arr[k][l];
-                }
-            }
-            System.out.println();
         }
 
         return answer;
-
     }
-
 }
