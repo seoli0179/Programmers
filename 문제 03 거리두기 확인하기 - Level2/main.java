14bcb8ed53baf8789f8568a43a77b52915395451
김태수 <seoli0179@kakao.com>
2023-12-20 11:39:25 +0900

    Update main.java

--- 문제 03 거리두기 확인하기 - Level2/main.java
+++ 문제 03 거리두기 확인하기 - Level2/main.java
@@ -1,80 +1,78 @@
-import java.util.*;
-
 // https://school.programmers.co.kr/learn/courses/30/lessons/81302
-// tip: each public class is put in its own file
 public class main
 {
     // tip: arguments are passed via the field below this editor
     public static void main(String[] args)
     {
-        String[][] places = new String[][]{{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"}, {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"}, {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"}, {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
+        
         Solution s = new Solution();
-        for(int i=0;i<5;i++){
-                System.out.println(s.cal(places[i]));
+        int[] answer = s.solution(new String[][]{{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"}, {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"}, {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"}, {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}});
+        for(int i : answer){
+            System.out.println(i+",");
         }
     }
 }
 
 class Solution {
+
+    private static final int[] dx = {0, -1, 1, 0};
+    private static final int[] dy = {-1, 0, 0, 1};
+
     public int[] solution(String[][] places) {
-        int[] answer = {};
+        int[] answer = new int[places.length];
+
+        for(int i = 0; i < answer.length; i++) {
+            String[] place = places[i];
+            char[][] room = new char[place.length][];
+            for(int j = 0; j < room.length; j++) {
+                room[j] = place[j].toCharArray();
+            }
+            if(isDistanced(room)) answer[i] = 1;
+            else answer[i] = 0;
+        }
+
+
         return answer;
     }
 
-    public int cal(String[] place) {
-        char[][] charArr = new char[5][5];
+    private boolean isDistanced(char[][] room) {
 
-        for(int i=0;i<place.length;i++){
-            charArr[i] = place[i].toCharArray();
-            
-        }
-
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                if(charArr[i][j]=='P'){
-                    for(int k=i;k<5;k++){
-                        for(int l=j+1;l<5;l++){
-                            if(charArr[k][l]=='P'){
-                                int r1 = i + 1;
-                                int c1 = j + 1;
-                                int r2 = k + 1;
-                                int c2 = l + 1;
-                                int distance = Math.abs(r1 - r2) + Math.abs(c1 - c2);
-                                //System.out.print(distance + " ");
-                                if(distance <= 2){
-                                    if(distance == 2){
-                                        if(i == k){
-                                            if(charArr[k][l-1] != 'X'){
-                                                return 0;
-                                            }
-                                        }else if(j == l){
-                                            if(charArr[k-1][l] != 'X'){
-                                                return 0;
-                                            }
-                                        }else{
-                                            if(j < l){
-                                                if(charArr[i][j+1] != 'X' || charArr[i+1][j] != 'X'){
-                                                    return 0;
-                                                }
-                                            }else{
-                                                if(charArr[i][j-1] != 'X' || charArr[i+1][j] != 'X'){
-                                                    return 0;
-                                                }
-                                            }
-                                        }
-                                    }
-                                    else{
-                                        return 0;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+        for(int y = 0; y < room.length; y++) {
+            for (int x = 0; x < room[y].length; x++) {
+                if(room[y][x] != 'P') continue;
+                if(!isDistanced(room, x, y)) return false;
             }
         }
+        return true;
+    }
 
-        return 1;
+    private boolean isDistanced(char[][] room, int x, int y) {
+
+        for(int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if(ny < 0 || ny >= room.length) continue;
+            if(nx < 0 || nx >= room[ny].length) continue;
+            if(room[ny][nx] == 'P') return false;
+            if(room[ny][nx] == 'O'){
+                if(isNextToValunteer(room, nx, ny, 3-d)) return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isNextToValunteer(char[][] room, int x, int y, int exclude) {
+
+        for(int d = 0; d < 4; d++) {
+            if (d == exclude) continue;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if(ny < 0 || ny >= room.length) continue;
+            if(nx < 0 || nx >= room[ny].length) continue;
+            if (room[ny][nx] == 'P') return true;
+        }
+        return false;
 
     }
+
 }
